ba79be07b12a79f76fb5366bd68a08a4e27a7ec0
fbwlsgh93 <42794341+fbwlsgh93@users.noreply.github.com>
2022-05-03 17:07:46 +0900

    call, vibe, foreground service

---
+++ Android/call/app/src/main/java/com/example/call/Actions.kt
@@ -0,0 +1,11 @@
+package com.example.call
+
+object Actions {
+    private const val prefix = "com.example.call.action."
+    const val MAIN = prefix + "main"
+    const val PREV = prefix + "prev"
+    const val NEXT = prefix + "next"
+    const val PLAY = prefix + "play"
+    const val START_FOREGROUND = prefix + "startforeground"
+    const val STOP_FOREGROUND = prefix + "stopforeground"
+}
