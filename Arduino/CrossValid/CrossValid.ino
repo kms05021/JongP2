7bd0a4f7d4ce6738cf1a53ad4c70071c1da3f48e
kms05021 <33238902+kms05021@users.noreply.github.com>
2022-05-15 17:10:24 +0900

    Update Comment
    Comment (Wind) Added

--- Arduino/CrossValid/CrossValid.ino
+++ Arduino/CrossValid/CrossValid.ino
@@ -1,30 +1,29 @@
 #include <SoftwareSerial.h>
-// Wind MACRO
+// 기류 센서 매크로
 #define Alert 5000
 #define Warning 10000
 #define Emergency 15000
-// Rubber band MACRO
+// 장력 고무줄 센서 매크로
 #define BUFFERSIZE 50
 #define TOLERANCE 0.988
 #define ALERTTIME 5000
 #define WARNINGTIME 10000
 #define EMERGENCYTIME 15000
 
-// Bluetooth global var
+// 블루투스 모듈 전역 변수
 const int pinTx = 7;  // 블루투스 TX 연결 핀 번호
 const int pinRx = 6;  // 블루투스 RX 연결 핀 번호
 SoftwareSerial   bluetooth( pinTx, pinRx );
 
-// Wind global var
-const float zeroWindAdjustment =  .2; // negative numbers yield smaller wind speeds and vice versa.
-int TMP_Therm_ADunits;  //temp termistor value from wind sensor
-float RV_Wind_ADunits;    //RV output from wind sensor 
-float RV_Wind_Volts;
-unsigned long lastMillis;
-int TempCtimes100;
-float zeroWind_ADunits;
-float zeroWind_volts;
-float WindSpeed_MPH;
+// 기류 센서 전역 변수
+const float zeroWindAdjustment =  .2; // 영점 보정 변수
+int TMP_Therm_ADunits;                // 온도 측정 값
+float RV_Wind_ADunits;                // RV 출력 값 (측정 값)
+float RV_Wind_Volts;                  // 전압 단위 RV 출력 값
+int TempCtimes100;                    // 섭씨 온도 변수
+float zeroWind_ADunits;               // 영점 측정 값 (초기 값)
+float zeroWind_volts;                 // 전압 단위 영점 측정 값
+float WindSpeed_MPH;                  // 바람 속도
 
 int inBoundW = 1;
 int isCountingW = 0;
@@ -35,7 +34,7 @@ int noWind = 0;
 
 String myString = "";
 
-// Rubber band global var
+// 장력 고무줄 센서 전역 변수
 int rubberValues[BUFFERSIZE] = {0,};
 int lastIndex = 0;
 int isCounting = 0;
@@ -46,43 +45,43 @@ int validBandA = 0;
 int validBandC = 0;
 int lowMovement = 0;
 
-// Delta Time global var
+// 델타 타임 변수
 unsigned long startTime=0;
 unsigned long curTime=0;
 
-// Level Flag
+// 위험도 레벨 플래그 (루프 당 1회 출력 체크)
 int flag1 = 0;
 int flag2 = 0;
 int flag3 = 0;
 
-// Reset Function
+// 초기화 콜백
 void resetFunc()
 {
-  asm volatile ("jmp 0");
+  asm volatile ("jmp 0");   // 처음 줄로 점프
 }
  
 void  setup()
 {
-  bluetooth.begin(9600);  // 블루투스 통신 초기화 (속도= 9600 bps)
+  bluetooth.begin(9600);          // 블루투스 통신 초기화 (속도= 9600 bps)
   Serial.begin(9600);
   while(1)
   {
     while(bluetooth.available())  //mySerial에 전송된 값이 있으면
     {
       char myChar = (char)bluetooth.read();  //mySerial int 값을 char 형식으로 변환
-      myString+=myChar;   //수신되는 문자를 myString에 모두 붙임 (1바이트씩 전송되는 것을 연결)
-      delay(10);           //수신 문자열 끊김 방지
+      myString+=myChar;                      //수신되는 문자를 myString에 모두 붙임 (1바이트씩 전송되는 것을 연결)
+      delay(10);                             //수신 문자열 끊김 방지
     }
-    if(!myString.equals(""))  //myString 값이 있다면
+    if(!myString.equals(""))                    //myString 값이 있다면
     {
       Serial.println("input value: "+myString); //시리얼모니터에 myString값 출력
-      if(myString.equals("START"))
+      if(myString.equals("START"))              //START 버튼이 눌렸을 경우
       {
-        myString="";  //myString 변수값 초기화
+        myString="";                            //myString 변수값 초기화
         break;
       }
     }
-    // Rubber band set up
+    // 장력 고무줄 센서 세팅 (초기 BUFFERSIZE 값 만큼 저장)
     for(int i = 0; i < BUFFERSIZE; i++)
     {
     int val = analogRead(A5);
@@ -111,24 +110,19 @@ void  loop()
     char toSend = (char)Serial.read();
     bluetooth.print(toSend);
   }
+
+  // 기류 센서 측정 및 연산
+  // 센서로 부터 Raw Data 입력 받기
   TMP_Therm_ADunits = analogRead(A0);
   RV_Wind_ADunits = analogRead(A1);
+  // 측정값 전압 단위로 변경
   RV_Wind_Volts = (RV_Wind_ADunits *  0.0048828125);
+  // 온도 측정 및 초기 바람의 양 계산 (영점용)
   TempCtimes100 = (0.005 *((float)TMP_Therm_ADunits * (float)TMP_Therm_ADunits)) - (16.862 * (float)TMP_Therm_ADunits) + 9075.4;  
-  zeroWind_ADunits = -0.0006*((float)TMP_Therm_ADunits * (float)TMP_Therm_ADunits) + 1.0727 * (float)TMP_Therm_ADunits + 47.172;  //  13.0C  553  482.39
-  zeroWind_volts = (zeroWind_ADunits * 0.0048828125) - zeroWindAdjustment; 
+  zeroWind_ADunits = -0.0006*((float)TMP_Therm_ADunits * (float)TMP_Therm_ADunits) + 1.0727 * (float)TMP_Therm_ADunits + 47.172;
+  zeroWind_volts = (zeroWind_ADunits * 0.0048828125) - zeroWindAdjustment;
+  // 속도 값 계산
   WindSpeed_MPH =  pow(((RV_Wind_Volts - zeroWind_volts) /.2300) , 2.7265);
-  //Serial.print("  TMP volts ");
-  //Serial.print(TMP_Therm_ADunits * 0.0048828125);
-  //Serial.print(" RV volts ");
-  //Serial.print((float)RV_Wind_Volts);
-  //Serial.print("\t  TempC*100 ");
-  //Serial.print(TempCtimes100 );
-  //Serial.print("   ZeroWind volts ");
-  //Serial.print(zeroWind_volts);
-  //Serial.print("   WindSpeed MPH ");
-  //Serial.println((float)WindSpeed_MPH);
-  //bluetooth.println((float)WindSpeed_MPH);
 
   // STOP Check
     while(bluetooth.available())  //mySerial에 전송된 값이 있으면
@@ -147,24 +141,24 @@ void  loop()
       }
     }
   
-  // Wind Check
-  if(WindSpeed_MPH < 2 || isnan(WindSpeed_MPH))
+  // 기류 센서 검사
+  if(WindSpeed_MPH < 2 || isnan(WindSpeed_MPH)) // 범위 밖의 값이 측정되거나 기준 값 미만이면
   {
-    inBoundW = 0;
+    inBoundW = 0;                               // 바운더리 플래그를 0으로 세팅
   }
   else
   {
     inBoundW = 1;
   }
 
-  if(!inBoundW)
+  if(!inBoundW)                                 // 바운더리 플래그가 0인 경우 (호흡이 기준 속도 미만일 경우)
     {
-      if(isCountingW)
+      if(isCountingW)                           // 카운팅 플래그가 1인 경우 (무호흡 상태가 유지되는 경우)
       {
-        curTime = millis();
+        curTime = millis();                     // 현재 시간을 측정하여 시간 차 비교
         if(curTime - startTime >= Alert && curTime - startTime < Warning)
         {
-          if(validVibe==0)
+          if(validVibe==0)                      // 진동 플래그 세팅
           {
             //bluetooth.println("LEVEL1");
             Serial.println("Vibration!!");
@@ -176,7 +170,7 @@ void  loop()
         }
         else if(curTime - startTime >= Warning && curTime - startTime < Emergency)
         {
-          if(validAlarm==0)
+          if(validAlarm==0)                      // 알람 플래그 세팅
           {
             //bluetooth.println("LEVEL2");
             Serial.println("Alarm!!");
@@ -187,7 +181,7 @@ void  loop()
         }
         else if(curTime - startTime >= Emergency)
         {
-          if(validCall==0)
+          if(validCall==0)                      // 전화 플래그 세팅
           {
             //bluetooth.println("LEVEL3");
             Serial.println("Call!!");
@@ -197,15 +191,15 @@ void  loop()
           }
         }
       }
-      else
+      else                    // 최초 무호흡 상태 돌입 시
       {
-        isCountingW = 1;
-        startTime = millis();
+        isCountingW = 1;      // 카운팅 플래그 1로 세팅
+        startTime = millis(); // 카운트 시작
       }
     }
-    else
+    else                      // 바운더리 플래그가 0이 아닌 경우 (정상적인 호흡 상태일 경우)
     {
-      if(isCountingW)
+      if(isCountingW)         // 카운팅 플래그가 세팅 되어 있다면 모든 플래그 0으로 초기화
       {
         isCountingW = 0;
         startTime = curTime = 0;
@@ -296,8 +290,10 @@ void  loop()
       launchTime = millis();  
     }
   }
+
+  // 기류 센서와 장력 고무줄 센서 값 확인 후 레벨 전송
   if((noWind == 1 && lowMovement >= 1)||(noWind >= 1 && lowMovement == 1))
-  {
+  {// 심각도 레벨 1인 경우
     if(flag1==0)
     {
       bluetooth.println("LEVEL1");
@@ -305,7 +301,7 @@ void  loop()
     }
   }
   else if((noWind == 2 && lowMovement >= 2)||(noWind >= 2 && lowMovement == 2))
-  {
+  {// 심각도 레벨 2인 경우
     if(flag2==0)
     {
       bluetooth.println("LEVEL2");
@@ -313,7 +309,7 @@ void  loop()
     }
   }
   else if(noWind == 3 && lowMovement == 3)
-  {
+  {// 심각도 레벨 3인 경우
     if(flag3==0)
     {
       bluetooth.println("LEVEL3");
