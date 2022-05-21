f20026d61a416e363c17020e561b0f32007629ae
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2022-05-21 13:59:06 +0900

    Revert "Revert "그래프 퍼센트를 횟수로 라벨링 변경 및 자잘한 수정""
    This reverts commit e94f8e5cdc3ba7dd9e1cbb8505971cda16a6806f.

--- Android/01AndroidBluetooth_MVVM/app/src/main/java/com/lilly/bluetoothclassic/fragment/WeekFragment.kt
+++ Android/01AndroidBluetooth_MVVM/app/src/main/java/com/lilly/bluetoothclassic/fragment/WeekFragment.kt
@@ -16,6 +16,7 @@ import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.lilly.bluetoothclassic.R
 import com.lilly.bluetoothclassic.log.LogDB
@@ -114,6 +115,8 @@ class WeekFragment : Fragment(), AdapterView.OnItemSelectedListener {
     private fun initBarChart() {
         //hide grid lines
         barChart.axisLeft.setDrawGridLines(false)
+        barChart.axisLeft.spaceBottom = 2f
+
         val xAxis: XAxis = barChart.xAxis
         xAxis.setDrawGridLines(false)
         xAxis.setDrawAxisLine(false)
@@ -161,6 +164,12 @@ class WeekFragment : Fragment(), AdapterView.OnItemSelectedListener {
         barDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
 
         val data = BarData(barDataSet)
+        var IntFormatter : ValueFormatter = object : ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                return value.toInt().toString() + "회"
+            }
+        }
+        data.setValueFormatter(IntFormatter)
         barChart.data = data
 
         barChart.invalidate()
